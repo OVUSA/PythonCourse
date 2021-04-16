60be54f7f750e7ac9fb8938e044a91e2c96602bd
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-04-16 18:26:52 -0500

    Update for.py

--- Chap06/for.py
+++ Chap06/for.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
-
-animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
+animals = ('bear', 'bunny', 'dog', 'cat', 'velociraptor')
 
 for pet in animals:
     print(pet)
+    if pet == 'dog':
+        continue
+    if pet == 'bunny':
+        break
+        print(pet)
 
+    else:
+        print("that is all of the animals")
+
+
+mylist = ("wine", "vodka", "beer", "blood", "cider")
+
+
+for drink in range(4):
+    print(drink)
