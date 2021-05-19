1a33a4f6616ee35c5ea70b03d880b6c8088fd3ac
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-05-18 19:29:12 -0500

    Check if King can change coordinate

---
+++ KingTurn.py
@@ -0,0 +1,9 @@
+class KingTurn
+
+x,y = int(input()),int(input())
+x1,y1 = int(input()),int(input())
+
+if -1<=(x1-x)<=1 and -1<= (y1-y) <=1:
+              print("YES")
+else:
+              print("NO")
